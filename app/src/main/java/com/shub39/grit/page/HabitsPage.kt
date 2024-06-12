fa9893537aaab1ed87a691a723eb39a77c4f54de
shub39 <cptnshubham39@gmail.com>
2024-06-12 08:52:49 +0530

    Fixed Status not appearing and minor UI improvements

--- app/src/main/java/com/shub39/grit/page/HabitsPage.kt
+++ app/src/main/java/com/shub39/grit/page/HabitsPage.kt
@@ -27,7 +27,6 @@ import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerState
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,9 +34,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
