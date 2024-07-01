aaac3a5c40220c3dcd48a1a9fb6597ab0233f579
shub39 <cptnshubham39@gmail.com>
2024-07-01 14:06:12 +0530

    update build number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 34
         versionCode = 1
-        versionName = "0.9.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
