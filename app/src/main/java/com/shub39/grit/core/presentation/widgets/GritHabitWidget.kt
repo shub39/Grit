c1e6a2e921ad8b26b5089cf6a45261746bae5357
shub39 <cptnshubham39@gmail.com>
2025-01-19 20:53:28 +0530

    :pencil2: Some Progress
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/src/main/java/com/shub39/grit/core/presentation/widgets/GritHabitWidget.kt
+++ app/src/main/java/com/shub39/grit/core/presentation/widgets/GritHabitWidget.kt
@@ -2,32 +2,43 @@ package com.shub39.grit.core.presentation.widgets
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.DataStoreFactory
+import androidx.datastore.core.Serializer
+import androidx.datastore.dataStoreFile
 import androidx.glance.GlanceId
-import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.appWidgetBackground
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
 import androidx.glance.appwidget.provideContent
-import androidx.glance.background
-import androidx.glance.layout.Alignment
-import androidx.glance.layout.Box
-import androidx.glance.layout.Column
-import androidx.glance.layout.fillMaxSize
-import androidx.glance.text.Text
+import androidx.glance.state.GlanceStateDefinition
 import com.shub39.grit.habits.data.database.HabitDatabase
 import com.shub39.grit.habits.data.database.HabitEntity
 import com.shub39.grit.habits.data.database.HabitStatusEntity
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+class GritWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = GritHabitWidget()
+}
 
 class GritHabitWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         val database = HabitDatabase.getDatabase(context)
-        val habit = database.habitDao().getAllHabits().firstOrNull()
+        val habits = database.habitDao().getAllHabits()
+
+        val habit = habits.firstOrNull()
+        val state = HabitWidgetState.getDataStore(context, id.toString()).data
         val habitStatuses = habit?.let { database.habitStatusDao().getStatusForHabit(it.id) }
 
         provideContent {
             GlanceTheme {
                 HabitMapWidget(
-                    habit = habit,
+                    habits = habits,
                     habitStatuses = habitStatuses
                 )
             }
@@ -36,31 +47,42 @@ class GritHabitWidget : GlanceAppWidget() {
 
     @Composable
     private fun HabitMapWidget(
-        habit: HabitEntity?,
+        habits: List<HabitEntity>,
         habitStatuses: List<HabitStatusEntity>?
     ) {
-        if (habit != null && habitStatuses != null) {
-            Column(
-                modifier = GlanceModifier
-                    .fillMaxSize()
-                    .appWidgetBackground(),
-                verticalAlignment = Alignment.Top,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(habit.title)
-                Text(habit.description)
-                Text(habitStatuses.size.toString())
-            }
-        } else {
-            Box(
-                modifier = GlanceModifier
-                    .fillMaxSize()
-                    .appWidgetBackground()
-                    .background(GlanceTheme.colors.background),
-                contentAlignment = Alignment.Center
-            ) {
-                Text("Habit Does Not Exist!! :'(")
-            }
+
+    }
+}
+
+object HabitWidgetState : GlanceStateDefinition<Long> {
+
+    private const val DATASTORE_FILE_NAME_PREFIX = "habit_widget_id_"
+
+    override suspend fun getDataStore(context: Context, fileKey: String): DataStore<Long> =
+        DataStoreFactory.create(
+            serializer = LongSerializer(defaultValue = 0L),
+            produceFile = { getLocation(context, fileKey) }
+        )
+
+    override fun getLocation(context: Context, fileKey: String): File =
+        context.dataStoreFile(DATASTORE_FILE_NAME_PREFIX + fileKey.lowercase())
+
+}
+
+private class LongSerializer(override val defaultValue: Long) : Serializer<Long> {
+    override suspend fun readFrom(input: InputStream): Long {
+        return try {
+            DataInputStream(input).use { it.readLong() }
+        } catch (e: IOException) {
+            defaultValue
+        }
+    }
+
+    override suspend fun writeTo(t: Long, output: OutputStream) {
+        try {
+            DataOutputStream(output).use { it.writeLong(t) }
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 }
--- app/src/main/java/com/shub39/grit/core/presentation/widgets/GritWidgetReceiver.kt
+++
@@ -1,8 +0,0 @@
-package com.shub39.grit.core.presentation.widgets
-
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-
-class GritWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget: GlanceAppWidget = GritHabitWidget()
-}
