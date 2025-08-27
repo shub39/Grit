861a94669292f537b2ea12b3fbf3dbc723e3350a
shub39 <cptnshubham39@gmail.com>
2025-08-27 16:09:08 +0530

    :wrench: bumped version
    Signed-off-by: shub39 <cptnshubham39@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 }
 
 val appName = "Grit"
-val appVersionCode = 4300
-val appVersionName = "4.3.0"
+val appVersionCode = 4310
+val appVersionName = "4.3.1"
 
 android {
     namespace = "com.shub39.grit"
