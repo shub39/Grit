637872cabbb62fe47b5b9b6ddc4bb688c6751965
Madhan S <madhan_selvaraj1@trimble.com>
2026-01-23 23:59:39 +0530

    AutoFocus New task input and Add category input when opened
    Dialog (Bottom-sheet in this case) with single input field as the
    primary action can be auto focused once the dialog is opened. This
    greatly enhances the UX by reducing multiple touch/click actions. The
    same is implemented in Edit Category name dialog, but missing in Add new
    category dialog and Add/Edit Task dialog

--- shared/core/src/commonMain/kotlin/com/shub39/grit/core/tasks/presentation/ui/section/TaskList.kt
+++ shared/core/src/commonMain/kotlin/com/shub39/grit/core/tasks/presentation/ui/section/TaskList.kt
@@ -63,6 +63,7 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.animateFloatingActionButton
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -70,7 +71,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.style.TextAlign
@@ -99,6 +103,7 @@ import grit.shared.core.generated.resources.edit_categories
 import grit.shared.core.generated.resources.items_completed
 import grit.shared.core.generated.resources.reorder_tasks
 import grit.shared.core.generated.resources.tasks
+import kotlinx.coroutines.delay
 import org.jetbrains.compose.resources.stringResource
 import sh.calvin.reorderable.ReorderableItem
 import sh.calvin.reorderable.rememberReorderableLazyListState
@@ -723,6 +728,15 @@ private fun AddCategorySheet(
     var name by remember { mutableStateOf("") }
 
     GritBottomSheet(onDismissRequest = onDismiss) {
+        val keyboardController = LocalSoftwareKeyboardController.current
+        val focusRequester = remember { FocusRequester() }
+
+        LaunchedEffect(Unit) {
+            delay(200)
+            focusRequester.requestFocus()
+            keyboardController?.show()
+        }
+
         Icon(imageVector = Icons.Rounded.Add, contentDescription = "Add")
         Text(
             text = stringResource(Res.string.add_category),
@@ -738,7 +752,7 @@ private fun AddCategorySheet(
                 imeAction = ImeAction.Done
             ),
             singleLine = true,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier.fillMaxWidth().focusRequester(focusRequester)
         )
         Button(
             onClick = { onAddCategory(Category(name = name, color = CategoryColors.GRAY.color)) },
