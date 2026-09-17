a5a4e2db56d9de8968b7369673d8671aa4c9bcf5
shub39 <cptnshubham39@gmail.com>
2026-09-17 08:16:49 +0530

    :wrench: fix(web demo): fixed unresponsive web demo

--- webDemo/src/commonMain/kotlin/com/shub39/grit/web_demo/di/AppModule.kt
+++ webDemo/src/commonMain/kotlin/com/shub39/grit/web_demo/di/AppModule.kt
@@ -18,6 +18,8 @@ package com.shub39.grit.web_demo.di
 
 import com.shub39.grit.shared.ui.di.UIModules
 import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Configuration
 import org.koin.core.annotation.Module
 
+@Configuration
 @Module(includes = [UIModules::class]) @ComponentScan("com.shub39.grit.web_demo") class AppModule
